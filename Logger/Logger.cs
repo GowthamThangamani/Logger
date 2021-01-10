9750a19cfc7de4c4a5cd9d293b27ae600e4b3efb
Gowtham T <vtgowtham1993@gmail.com>
2021-01-10 19:53:38 +0530

    added Logger.cs

---
+++ Logger/Logger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    public class Logger
+    {
+        // LogLevel
+        // filter logLevel.
+        // BaseName
+        // type of Message
+        //      - Normal Message
+        //      - Error Message
+        //      - Exception Message
+        // Received those log.
+
+        public enum eLogLevel
+        {
+            verbose,
+            debug,
+            info,
+            warning,
+            error,
+        }
+
+        string message;
+        string error_message;
+        string stack_trace;
+        private readonly string baseName;
+
+        public Logger(string BaseName = "")
+        {
+            baseName = BaseName;
+        }
+
+        public void V(string message,
+                      string error_message = null,
+                      string stack_trace = null)
+        {
+
+        }
+
+        public void D(string message,
+                     string error_message = null,
+                     string stack_trace = null)
+        {
+
+        }
+
+    }
+
+    class LogHandler
+    {
+
+    }
+}
--- Logger/Program.cs
+++ Logger/Program.cs
@@ -2,6 +2,25 @@
 
 namespace Logger
 {
+    //The logger is one important assert during development.We are going to create a Logger library which is used to collect log from the various application.From mobile, Web, even from windows application. 
+    //We are going to create 4 C# Library for the logger.
+		  //  - Log Handler which collect all log.
+		  //  - Log Writer, which will write in files, database, or even in API.
+		  //  - Log Viewer, to visualize the log, in all different medium.
+		  //  - Log Uploader, which uploads the log to the server.
+    //First, we are going to write everything in c#, in .NetStarndard.
+    //Later we will develop for the dart. 
+
+
+    //My suggestion is to create as LogHandler, LogWritter, LogViewer as an opensource project.
+    //So that 
+		  //  - Anyone can edit it, we can decide whether to accept or not.
+ 		 //   - we will not lose any code. 
+ 		 //   - We will get a constant reminder from the public about bugs.
+ 		 //   - We will get the contribution from the community.
+ 		 //   - We can make this system more stable.
+
+
     class Program
     {
         static void Main(string[] args)
